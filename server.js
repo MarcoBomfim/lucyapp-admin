77f0bf0e514aec71b4fbab4d29770a2225756f19
MarcoBomfim <marcoabomfim3@gmail.com>
2021-10-14 20:24:55 -0300

    chore: fix outdated yarn, add issing cors, add dist to gitignore

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const serveStatic = require("serve-static")
 const path = require('path');
 
 const app = express();
+app.use(cors());
 app.use(serveStatic(path.join(__dirname, 'dist')));
 
 const port = process.env.PORT || 8000;
